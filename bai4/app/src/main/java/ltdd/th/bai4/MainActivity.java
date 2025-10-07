cada23b4d1cb5ca7a1eea039338d2f573f84f8c6
Sylein1 <khanhsky2k5nam@gmail.com>
2025-10-07 20:40:07 +0700

    bai 4 xong

--- bai4/app/src/main/java/ltdd/th/bai4/MainActivity.java
+++ bai4/app/src/main/java/ltdd/th/bai4/MainActivity.java
@@ -2,6 +2,7 @@ package ltdd.th.bai4;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,22 +17,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Intent intent = new Intent(MainActivity.this, AccountActivity.class);
-        Bundle bundle = new Bundle();
-        bundle.putString("fullName", "Nguyễn Văn A");
-        bundle.putString("email", "vana@gmail.com");
-        bundle.putString("phone", "0123456789");
-        bundle.putString("branch", "Khách sạn Đà Nẵng");
-        bundle.putString("joinDate", "12/02/2024");
-        bundle.putString("status", "Đang hoạt động");
-        intent.putExtras(bundle);
-        startActivity(intent);
+        btnLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("fullName", "Nguyễn Văn A");
+            bundle.putString("email", "vana@gmail.com");
+            bundle.putString("phone", "0123456789");
+            bundle.putString("branch", "Khách sạn Đà Nẵng");
+            bundle.putString("joinDate", "12/02/2024");
+            bundle.putString("status", "Đang hoạt động");
+            intent.putExtras(bundle);
+            startActivity(intent);
+        });
 
     }
 }
